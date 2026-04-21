00cf73d2e879f264e142f4cc9035e16ca559a3f7
vdsn0905 <vdsn0905@gmail.com>
2026-04-21 21:33:27 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
-    <main>
+    <main className="w-full h-screen">
       <Spline 
         scene="https://prod.spline.design/mtIn-8DmjK-UW690/scene.splinecode" 
       />
