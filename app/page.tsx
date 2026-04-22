ed0425207f04038115283fb73053f208dccd378b
Sanket Patil <sanketpatil574918@gmail.com>
2026-04-22 09:22:05 +0530

    Adjust main element size for responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
-    <main className="w-full h-screen">
+    <main className="w-full h-screen sm:w-auto sm:h-auto">
       <Spline 
         scene="https://prod.spline.design/mtIn-8DmjK-UW690/scene.splinecode" 
       />
